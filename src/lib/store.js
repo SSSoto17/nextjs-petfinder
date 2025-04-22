1683e787e701bb947d437c4bccb962eb9405928f
Sabrina Sòlborg Soto <saso0001@stud.kea.dk>
2025-04-22 10:33:32 +0200

    removes redundancy

--- src/lib/store.js
+++ src/lib/store.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist, createJSONStorage } from "zustand/middleware";
+import { persist } from "zustand/middleware";
 
 const useFilterStore = create((set) => ({
   activeFilter: "All",
